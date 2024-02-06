3493412e4263d9076b9c88729a0fb4cb256fe418
sadiabbasi <sadiaabbas326@gmail.com>
2024-02-06 14:13:27 +0300

    update configured chains

--- src/config/configuredChains.ts
+++ src/config/configuredChains.ts
@@ -3,14 +3,14 @@ import Telos from 'src/config/chains/telos';
 import UX from 'src/config/chains/ux';
 import Wax from 'src/config/chains/wax';
 
-import TelosTestnet from 'src/config/chains/telos-testnet';
+import PangeaTestnet from 'src/config/chains/pangea-testnet';
 import Jungle from 'src/config/chains/jungle';
 
 import { ChainsConfig } from 'src/types/ChainsConfig';
 
 const chains: ChainsConfig = {
     mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax')],
-    testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet')],
+    testnets: [new Jungle('jungle'), new PangeaTestnet('pangea-testnet')],
 };
 
 export default chains;
