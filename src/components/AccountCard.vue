eadc5e76b7eb94f8f59d0e39176ab6ca5f036de0
sadiabbasi <sadiaabbas326@gmail.com>
2024-02-10 15:12:10 +0300

    added some code comment out

--- src/components/AccountCard.vue
+++ src/components/AccountCard.vue
@@ -236,11 +236,11 @@ export default defineComponent({
         onMounted(async () => {
             usdPrice.value = await chain.getUsdPrice();
             await loadAccountData();
-            await store.dispatch('account/updateRexData', {
-                account: props.account,
-            });
+            // await store.dispatch('account/updateRexData', {
+            //     account: props.account,
+            // });
             loadSystemToken();
-            void store.dispatch('chain/updateRamPrice');
+            // void store.dispatch('chain/updateRamPrice');
         });
 
         watch(
@@ -248,9 +248,9 @@ export default defineComponent({
             async () => {
                 resetBalances();
                 await loadAccountData();
-                await store.dispatch('account/updateRexData', {
-                    account: props.account,
-                });
+                // await store.dispatch('account/updateRexData', {
+                //     account: props.account,
+                // });
             },
         );
 
