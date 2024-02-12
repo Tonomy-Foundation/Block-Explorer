9f7e9694760b608019ca5b5182af97657066fa85
sadiabbasi <sadiaabbas326@gmail.com>
2024-02-12 17:57:16 +0300

    change svg to png

--- src/config/chains/pangea-testnet/index.ts
+++ src/config/chains/pangea-testnet/index.ts
@@ -149,11 +149,11 @@ export default class PangeaTestnet extends BaseChain {
     }
 
     getLargeLogoPath(): string {
-        return 'chains/pangea/pangea-large-logo.svg';
+        return 'chains/pangea/pangea-large-logo.png';
     }
 
     getSmallLogoPath(): string {
-        return 'chains/pangea/pangea-small-logo.svg';
+        return 'chains/pangea/pangea-small-logo.png';
     }
 
     getMapDisplay(): boolean {
