6dd72d0b44973a1d38a36a199378108bf2f55e6c
sadiabbasi <sadiaabbas326@gmail.com>
2024-02-12 16:36:45 +0300

    comment dispatch in resources

--- src/store/contract/actions.ts
+++ src/store/contract/actions.ts
@@ -3,10 +3,10 @@ import { StateInterface } from 'src/store/index';
 import { ContractStateInterface } from 'src/store/contract/state';
 
 export const actions: ActionTree<ContractStateInterface, StateInterface> = {
-    async setContract({ dispatch, commit }, contractAddress) {
-        commit('setContract', contractAddress);
-        await dispatch('getContractInfo', contractAddress);
-    },
+    // async setContract({ dispatch, commit }, contractAddress) {
+    //     commit('setContract', contractAddress);
+    //     await dispatch('getContractInfo', contractAddress);
+    // },
     getContractInfo({ commit }) {
         const response: { creator: string } = { creator: 'bob' }; //mock
         commit('setCreator', response.creator);
